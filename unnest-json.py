be15fe7a984cb8b86e988f7f898b709cf947ae64
JeremyJamesL <47485563+JeremyJamesL@users.noreply.github.com>
2022-09-01 14:50:22 +0100

    unnest JSON

---
+++ unnest-json.py
@@ -0,0 +1,21 @@
+import json
+# import logging
+# import csv
+from collections import Counter
+
+input_file_name = '' # input file path
+output_file_name = '' # output file path
+
+with open(input_file_name) as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
+
+newList = []
+
+for item in jsonObject:
+    if "list_item," in item: newList.append(item["field_to_unnest"])  
+
+
+with open(output_file_name, "w") as jsonFile:
+    json.dump(newList, jsonFile)
+    jsonFile.close()
